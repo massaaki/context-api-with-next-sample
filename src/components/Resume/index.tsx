85a3b717f82ffdc0ccaa60be3f091fb72d1c65e7
Mauricio Yochizumi <massaaki@Mauricios-MacBook-Pro.local>
2021-12-29 07:54:03 -0300

    feat: add % profit

--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -5,6 +5,8 @@ import * as S from './styles';
 
 export const Resume = () => {
   const { totalInvested, totalProfit } = useInvest();
+
+
   return (
     <S.Container>
       <S.Content>
@@ -12,6 +14,7 @@ export const Resume = () => {
           <ResumeItem title='Total' value={String((totalInvested + totalProfit).toFixed(2))} />
           <ResumeItem title='Invested' value={String(totalInvested.toFixed(2))} />
           <ResumeItem title='Profit' value={String(totalProfit.toFixed(2))} />
+          <ResumeItem title='%' value={totalInvested > 0 ? String(((totalProfit / totalInvested) * 100).toFixed(2)) + "%" : '0' + "%"} />
         </ul>
       </S.Content>
     </S.Container>
